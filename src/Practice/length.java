651b0349fdc74ff068943f83c97d23876385059b
Ranji <Ranji@Aanie>
2024-01-16 14:38:31 -0500

    Commiting program length.java

---
+++ src/Practice/length.java
@@ -0,0 +1,21 @@
+package Practice;
+
+public class length {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				String x = "Ambili";
+				System.out.println(x.length());
+				
+				char y[]= x.toCharArray();
+				int i=0;
+				
+				for(char c: x.toCharArray())
+				{
+					i++;
+				}
+				
+				System.out.println(i);
+	}
+
+}
