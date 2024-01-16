0bacbd07b844f68c6d9aa7596fb016d7c20b4e38
Ranji <Ranji@Aanie>
2024-01-16 15:26:04 -0500

    Commiting palindrome

---
+++ src/Practice/palindrome.java
@@ -0,0 +1,40 @@
+package Practice;
+
+public class palindrome {
+
+	public static void main(String[] args) {
+		
+		String str ="malayalam";
+		char x[] = str.toCharArray();
+		
+		int size = str.length();
+		char y[] = new char[size];
+		int i=0;
+		
+		while(i!=size)
+		{
+			y[i] = x[size-1-i];
+			i++;
+		}
+		
+		i=0;
+		while(i!=size)
+		{
+			if(x[i]!=y[i])
+			{
+				System.out.println("Not a palindrome"); 
+				System.exit(0); ;
+				
+			}
+			else
+			{
+				i++;
+				
+			}
+		}
+		
+		System.out.println("Palindrome");
+		
+	}
+
+}
